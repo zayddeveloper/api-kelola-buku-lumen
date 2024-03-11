e8fc2d1eea49a5b1702bb735ecf67467a6627bfc
KangZayd <msaida97@gmail.com>
2024-03-11 18:31:38 +0700

    setup prettier

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -8,31 +8,42 @@ use Barryvdh\DomPDF\Facade\Pdf;
 
 class BooksController extends CustomController
 {
-
     public function index()
     {
         try {
-            $date = $this->request->query->get('date');
-            $startDate = $this->request->query->get('start_date');
-            $endDate = $this->request->query->get('end_date');
-            $query = Books::with(['user']);
-            $data=[];
+            $date = $this->request->query->get("date");
+            $startDate = $this->request->query->get("start_date");
+            $endDate = $this->request->query->get("end_date");
+            $query = Books::with(["user"]);
+            $data = [];
             if (!$date) {
                 if (!$startDate && !$endDate) {
-                    $data = $query->orderBy('created_at', 'DESC')->get();
+                    $data = $query->orderBy("created_at", "DESC")->get();
                 } else {
-                    $data = $query->whereDate('created_at', '<=', $endDate)
-                        ->whereDate('created_at', '>=', $startDate)->orderBy('created_at', 'DESC')->get();
+                    $data = $query
+                        ->whereDate("created_at", "<=", $endDate)
+                        ->whereDate("created_at", ">=", $startDate)
+                        ->orderBy("created_at", "DESC")
+                        ->get();
                 }
             } else {
-                $data = $query->whereDate('created_at', '=', $date)->orderBy('created_at', 'DESC')->get();
+                $data = $query
+                    ->whereDate("created_at", "=", $date)
+                    ->orderBy("created_at", "DESC")
+                    ->get();
             }
             if ($data->isEmpty()) {
-                return $this->jsonResponse('Data ' . $date.' not found! ', 404);
+                return $this->jsonResponse(
+                    "Data " . $date . " not found! ",
+                    404
+                );
             }
-            return $this->jsonResponse('success', 200, $data);
+            return $this->jsonResponse("success", 200, $data);
         } catch (\Throwable $e) {
-            return $this->jsonResponse('internal server error ' . $e->getMessage(), 500);
+            return $this->jsonResponse(
+                "internal server error " . $e->getMessage(),
+                500
+            );
         }
     }
 
@@ -41,35 +52,41 @@ class BooksController extends CustomController
         try {
             $body = $this->parseRequestBody();
             $data = [
-                'user_id' => $body['user_id'],
-                'judul' => $body['judul'],
-                'penulis' => $body['penulis'],
-                'penerbit' => $body['penerbit'],
-                'tahun_terbit' => $body['tahun_terbit'],
+                "user_id" => $body["user_id"],
+                "judul" => $body["judul"],
+                "penulis" => $body["penulis"],
+                "penerbit" => $body["penerbit"],
+                "tahun_terbit" => $body["tahun_terbit"],
             ];
             $add = Books::create($data);
-            return $this->jsonResponse('success', 200, $add);
+            return $this->jsonResponse("success", 200, $add);
         } catch (\Throwable $e) {
-            return $this->jsonResponse('internal server error ' . $e->getMessage(), 500);
+            return $this->jsonResponse(
+                "internal server error " . $e->getMessage(),
+                500
+            );
         }
     }
 
     public function getByID($id)
     {
         try {
-            $data = Books::with([])->where('id', '=', $id)->first();
+            $data = Books::with([])->where("id", "=", $id)->first();
             if (!$data) {
-                return $this->jsonResponse(' not found', 404);
+                return $this->jsonResponse(" not found", 404);
             }
-            if ($this->request->method() === 'POST') {
+            if ($this->request->method() === "POST") {
                 return $this->patch($data);
             }
-            if ($this->request->method() === 'DELETE') {
+            if ($this->request->method() === "DELETE") {
                 return $this->delete($data);
             }
-            return $this->jsonResponse('success', 200, $data);
+            return $this->jsonResponse("success", 200, $data);
         } catch (\Throwable $e) {
-            return $this->jsonResponse('internal server error ' . $e->getMessage(), 500);
+            return $this->jsonResponse(
+                "internal server error " . $e->getMessage(),
+                500
+            );
         }
     }
 
@@ -78,16 +95,19 @@ class BooksController extends CustomController
         try {
             $body = $this->parseRequestBody();
             $data_request = [
-                'kategori_id' => $body['kategori_id'] ?? 1,
-                'judul' => $body['judul'],
-                'penulis' => $body['penulis'],
-                'penerbit' => $body['penerbit'],
-                'tahun_terbit' => $body['tahun_terbit'],
+                "kategori_id" => $body["kategori_id"] ?? 1,
+                "judul" => $body["judul"],
+                "penulis" => $body["penulis"],
+                "penerbit" => $body["penerbit"],
+                "tahun_terbit" => $body["tahun_terbit"],
             ];
             $data->update($data_request);
-            return $this->jsonResponse('success', 200);
+            return $this->jsonResponse("success", 200);
         } catch (\Throwable $e) {
-            return $this->jsonResponse('internal server error ' . $e->getMessage(), 500);
+            return $this->jsonResponse(
+                "internal server error " . $e->getMessage(),
+                500
+            );
         }
     }
 
@@ -95,9 +115,12 @@ class BooksController extends CustomController
     {
         try {
             $data->delete();
-            return $this->jsonResponse('success', 200);
+            return $this->jsonResponse("success", 200);
         } catch (\Throwable $e) {
-            return $this->jsonResponse('internal server error ' . $e->getMessage(), 500);
+            return $this->jsonResponse(
+                "internal server error " . $e->getMessage(),
+                500
+            );
         }
     }
 
@@ -105,21 +128,29 @@ class BooksController extends CustomController
     {
         try {
             Books::truncate();
-            return $this->jsonResponse('success', 200);
+            return $this->jsonResponse("success", 200);
         } catch (\Throwable $e) {
-            return $this->jsonResponse('internal server error ' . $e->getMessage(), 500);
+            return $this->jsonResponse(
+                "internal server error " . $e->getMessage(),
+                500
+            );
         }
     }
 
     public function cetakPdf()
     {
         try {
-            $data['data'] = Books::with(['user:id,nama'])->orderBy('created_at', 'DESC')->get();
-            $pdf = Pdf::loadview('books_pdf', $data);
-            return $pdf->download('laporan-pegawai-pdf'); // to download pdf
-//            return $pdf->stream(); // to show pdf on browser
+            $data["data"] = Books::with(["user:id,nama"])
+                ->orderBy("created_at", "DESC")
+                ->get();
+            $pdf = Pdf::loadview("books_pdf", $data);
+            return $pdf->download("laporan-pegawai-pdf"); // to download pdf
+            //            return $pdf->stream(); // to show pdf on browser
         } catch (\Throwable $e) {
-            return $this->jsonResponse('internal server error ' . $e->getMessage(), 500);
+            return $this->jsonResponse(
+                "internal server error " . $e->getMessage(),
+                500
+            );
         }
     }
 }
