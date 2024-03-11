15450a376f657cc8917bea249d4128ea31ecf0a7
KangZayd <msaida97@gmail.com>
2024-03-11 18:32:11 +0700

    setup prettier

--- resources/views/books_pdf.blade.php
+++ resources/views/books_pdf.blade.php
@@ -6,46 +6,46 @@
 <html>
 <head>
     <title>Membuat Laporan PDF Dengan DOMPDF Laravel</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<style type="text/css">
-    table tr td,
-    table tr th {
-        font-size: 9pt;
-    }
-</style>
-<center>
-    <h5>Membuat Laporan PDF Dengan DOMPDF Lumen</h4>
-</center>
-<br></br>
+    <style type="text/css">
+        table tr td,
+        table tr th {
+            font-size: 9pt;
+        }
 
-<table class='table table-bordered'>
-    <thead>
-    <tr>
-        <th>No</th>
-        <th>judul</th>
-        <th>penulis</th>
-        <th>penerbit</th>
-        <th>tahun terbit</th>
-        <th>Created By</th>
-    </tr>
-    </thead>
-    <tbody>
-    @php $i=1 @endphp
-    @foreach($data as $p)
-        <tr>
-            <td>{{ $i++ }}</td>
-            <td>{{$p->judul}}</td>
-            <td>{{$p->penulis}}</td>
-            <td>{{$p->penerbit}}</td>
-            <td>{{$p->tahun_terbit}}</td>
-            <td>{{$p->user->nama}}</td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+    </style>
+    <center>
+        <h5>Membuat Laporan PDF Dengan DOMPDF Lumen</h4>
+    </center>
+    <br></br>
+
+    <table class='table table-bordered'>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>judul</th>
+                <th>penulis</th>
+                <th>penerbit</th>
+                <th>tahun terbit</th>
+                <th>Created By</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $i=1 @endphp
+            @foreach($data as $p)
+            <tr>
+                <td>{{ $i++ }}</td>
+                <td>{{$p->judul}}</td>
+                <td>{{$p->penulis}}</td>
+                <td>{{$p->penerbit}}</td>
+                <td>{{$p->tahun_terbit}}</td>
+                <td>{{$p->user->nama}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 </body>
 </html>
